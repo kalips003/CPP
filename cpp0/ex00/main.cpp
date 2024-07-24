3b59342d3ba95ea14b65659752aef6d802be369a
Your Name <you@example.com>
2024-07-24 14:51:24 +0200

    Wed Jul 24 14:51:24 CEST 2024

--- cpp0/ex00/main.cpp
+++ cpp0/ex00/main.cpp
@@ -6,13 +6,27 @@
 /*   By: kalipso <kalipso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 14:17:39 by kalipso           #+#    #+#             */
-/*   Updated: 2024/07/24 14:27:17 by kalipso          ###   ########.fr       */
+/*   Updated: 2024/07/24 14:51:02 by kalipso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else {
+		for (int i = 1; i < argc; i++) {
+			for (int j = 0; argv[i][j] != '\0'; j++) {
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+			}
+			if (i < argc - 1) {
+				std::cout << ' ';
+			}
+		}
+		std::cout << std::endl;
+	}
+	return 0;
 }
