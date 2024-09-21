52705a8db9edfe643ca86db45fdef553463193d9
Your Name <you@example.com>
2024-09-21 22:43:32 +0200

    Sat Sep 21 22:43:32 CEST 2024

--- cpp0/ex01/Contact.cpp
+++ cpp0/ex01/Contact.cpp
@@ -14,22 +14,22 @@ Contact::Contact() {
 Contact::~Contact() {
 }
 
-//  SETTER
-void Contact::setFirstName(char *value) {
-    first_name = value;
-}
-//  SETTER
-void Contact::setAttribute(char *value) {
-    attribute = value;
-}
+/////	SETTER
+void  Contact::set_first_name(std::string str) {first_name = str;}
+void  Contact::set_last_name(std::string str) {last_name = str;}
+void  Contact::set_nickname(std::string str) {nickname = str;}
+void  Contact::set_darkest_secret(std::string str) {darkest_secret = str;}
+void  Contact::set_phone_number(std::string str) {phone_number = str;}
+/////	GETTER
+std::string Contact::get_first_name() const {return first_name;}
+std::string Contact::get_last_name() const {return last_name;}
+std::string Contact::get_nickname() const {return nickname;}
+std::string Contact::get_darkest_secret() const {return darkest_secret;}
+std::string Contact::get_phone_number() const {return phone_number;}
 
-//  GETTER
-int Contact::getAttribute() const {
-    return attribute;
-}
 
 ////////////////////////////////////////////////]
 //  OTHER FUNCTIONS
-void Contact::printAttribute() const {
-    std::cout << "The attribute is: " << attribute << std::endl;    
-}
+// void Contact::printAttribute() const {
+//     std::cout << "The attribute is: " << attribute << std::endl;    
+// }
--- cpp0/ex01/Contact.hpp
+++ cpp0/ex01/Contact.hpp
@@ -5,31 +5,39 @@
 
 class Contact {
 private:
-    /////   PRIVATE DATA
-    std::string first_name;
-    std::string last_name;
-    std::string nickname;
-    std::string darkest_secret;
-    std::string phone_number;
-
+	/////   PRIVATE DATA
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string darkest_secret;
+	std::string phone_number;
 
 public:
-    /////   PUBLIC STANDARD FUNCIONS
-    Contact();
-    Contact(int value);
-    Contact(const Contact& other);
-    ~Contact();
-    // Contact& operator=(const Contact& other);  // Copy assignment operator
+	/////   PUBLIC STANDARD FUNCIONS
+	Contact();
+	Contact(int value);
+	Contact(const Contact& other);
+	~Contact();
+	// Contact& operator=(const Contact& other);  // Copy assignment operator
 
-    /////   PUBLIC SETTER GETTER
-    void setFirstName(std::string phone_number value);    // Setter
-    int getAttribute() const;        // Getter
+	/////   PUBLIC SETTER
+	void set_first_name(std::string first_name);
+	void set_last_name(std::string last_name);
+	void set_nickname(std::string nickname);
+	void set_darkest_secret(std::string darkest_secret);
+	void set_phone_number(std::string phone_number);
+	/////   PUBLIC GETTER
+	std::string get_first_name() const;
+	std::string get_last_name() const;
+	std::string get_nickname() const;
+	std::string get_darkest_secret() const;
+	std::string get_phone_number() const;
 
-    // OTHER PUBLIC FUNCTIONS
-    void printAttribute() const;
+	// OTHER PUBLIC FUNCTIONS
+	void printAttribute() const;
 
-    /////   PUBLIC DATA
-    std::string name;
+	/////   PUBLIC DATA
+	std::string name;
 
 };
 
