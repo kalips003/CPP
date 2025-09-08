5776f32110cb691c6a4b3b3d63a98ea6307da7f5
Adrien Gallon <agallon@f6r10s4.clusters.42paris.fr>
2025-09-08 20:56:40 +0200

    Mon Sep  8 08:56:40 PM CEST 2025

--- cpp5/ex03/main.cpp
+++ cpp5/ex03/main.cpp
@@ -10,38 +10,23 @@
 // •ShrubberyCreationForm: 	sign 145, exec 137
 // •RobotomyRequestForm: 	sign 72, exec 45
 // •PresidentialPardonForm: sign 25, exec 5
+// { "Shrubbery creation", "Robotomy Request", "Presidential Pardon" };
 ///////////////////////////////////////////////////////////////////////////////]
 int main() {
-	try {
-		Bureaucrat boss("Boss", 1);
-		Bureaucrat jeff("Jeff", 50);
-		Bureaucrat intern(RED"Intern", 150);
-  
-		SCF tree("you");
-		RRF lobo("you");
-		PPF pardon("you");
 
-		std::cout << jeff << boss << intern << std::endl;
-
-		jeff.signForm(pardon);
-		boss.executeForm(pardon);
-		jeff.executeForm(pardon);
-		boss.signForm(pardon);
-		jeff.executeForm(pardon);
-		boss.executeForm(pardon);
-
-	} catch (std::exception &e) {
-		std::cout << "Exception caught: " << e.what() << std::endl;
-	}
 	Intern in;
 	AForm *a = in.makeForm("Shrubbery creation", "the world");
 	AForm *b = in.makeForm("Shrubbery creation", "yoiu");
 	AForm *c = in.makeForm("Shruery creation", "Your mom");
 
+std::cout << std::endl;///////////////////////////////////]
+
 	Bureaucrat boss("Boss", 1);
 
 	if (a) {boss.signForm(*a); boss.executeForm(*a);}
+std::cout << std::endl;///////////////////////////////////]
 	if (b) {boss.signForm(*b); boss.executeForm(*b);}
+std::cout << std::endl;///////////////////////////////////]
 	if (c) {boss.signForm(*c); boss.executeForm(*c);}
 	
 	return 0;
